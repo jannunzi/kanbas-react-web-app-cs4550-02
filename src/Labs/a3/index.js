461708cf65b8c21e3ca6865d6f73e8e29bc93d14
Jose Annunziato <jose.annunziato@sonos.com>
2023-10-19 11:38:49 -0400

    a4

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -3,10 +3,13 @@ import PathParameters from "./PathParameters";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoItem from "./todo/TodoItem";
 import TodoList from "./todo/TodoList";
+import { useSelector } from "react-redux";
 function Assignment3() {
+  const { todos } = useSelector((state) => state.todosReducer);
   return (
     <div>
       <h1>Assignment 3</h1>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
       <TodoList />
       <TodoItem todo={{ done: true, title: "TODO 123", status: "COMPLETE" }} />
       <TodoItem todo={{ done: true, title: "TODO 123", status: "COMPLETE" }} />
