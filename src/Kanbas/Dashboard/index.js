feadbc8ef133a97f8441743d5311bdf48e5cd7b4
Jose Annunziato <jose.annunziato@sonos.com>
2023-10-27 07:50:25 -0400

    dashboard state variable

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,11 +1,65 @@
 import { Link } from "react-router-dom";
 import db from "../Database";
 import "./index.css";
+import { React, useState } from "react";
+
 function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+  });
+  const deleteCourse = (courseId) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const addNewCourse = () => {
+    setCourses([...courses, { ...course, _id: new Date().getTime() }]);
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <button onClick={updateCourse}>Update</button>
+
+      <button onClick={addNewCourse}>Add</button>
+      <h5>Course</h5>
+      <input
+        value={course.name}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <input
+        value={course.number}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })}
+      />
+      <input
+        value={course.startDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
+      />
+      <input
+        value={course.endDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
+      />
+
       <div className="list-group">
         {courses.map((course) => (
           <Link
@@ -13,6 +67,24 @@ function Dashboard() {
             to={`/Kanbas/Courses/${course._id}`}
             className="list-group-item"
           >
+            <button
+              onClick={(event) => {
+                event.preventDefault();
+                deleteCourse(course._id);
+              }}
+            >
+              Delete
+            </button>
+
+            <button
+              onClick={(event) => {
+                event.preventDefault();
+                setCourse(course);
+              }}
+            >
+              Edit
+            </button>
+
             {course.name}
           </Link>
         ))}
